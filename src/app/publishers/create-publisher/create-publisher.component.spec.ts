0c8b4ee36d14caea2f33646be81f81782254807f
Rildo Alves <junior.alvesp99@gmail.com>
2020-11-01 18:59:24 -0300

    Cria os componentes de insercao

---
+++ src/app/publishers/create-publisher/create-publisher.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreatePublisherComponent } from './create-publisher.component';
+
+describe('CreatePublisherComponent', () => {
+  let component: CreatePublisherComponent;
+  let fixture: ComponentFixture<CreatePublisherComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreatePublisherComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreatePublisherComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/publishers/create-publisher/create-publisher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-publisher',
+  templateUrl: './create-publisher.component.html',
+  styleUrls: ['./create-publisher.component.css']
+})
+export class CreatePublisherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
