aad8907d9b82b016f3fcf4625cfa3746561ea87b
Rildo Alves <junior.alvesp99@gmail.com>
2020-11-01 15:42:06 -0300

    Criando os componentes necessarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { GamesComponent } from './games/games.component';
+import { PublishersComponent } from './publishers/publishers.component';
+import { GameComponent } from './games/game/game.component';
+import { PublisherComponent } from './publishers/publisher/publisher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsersComponent,
+    GamesComponent,
+    PublishersComponent,
+    GameComponent,
+    PublisherComponent,
   ],
   imports: [
     BrowserModule
